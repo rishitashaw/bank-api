1c687a074bd14cb2247aebe7a2c48a77b7b0f155
Rishita Shaw <rishitashaw49@gmail.com>
2023-06-10 18:13:37 +0530

    sqlc generated

--- database/sqlc/account.sql.go
+++ database/sqlc/account.sql.go
@@ -10,13 +10,9 @@ import (
 )
 
 const createAccount = `-- name: CreateAccount :one
-INSERT INTO accounts (
-  owner,
-  balance,
-  currency
-) VALUES (
-  $1, $2, $3
-) RETURNING id, owner, balance, currency, created_at
+INSERT INTO accounts (owner, balance, currency)
+VALUES ($1, $2, $3)
+RETURNING id, owner, balance, currency, created_at
 `
 
 type CreateAccountParams struct {
@@ -38,9 +34,20 @@ func (q *Queries) CreateAccount(ctx context.Context, arg CreateAccountParams) (A
 	return i, err
 }
 
+const deleteAccount = `-- name: DeleteAccount :exec
+DELETE FROM accounts WHERE id = $1
+`
+
+func (q *Queries) DeleteAccount(ctx context.Context, id int64) error {
+	_, err := q.db.ExecContext(ctx, deleteAccount, id)
+	return err
+}
+
 const getAccount = `-- name: GetAccount :one
-SELECT id, owner, balance, currency, created_at FROM accounts
-WHERE id = $1 LIMIT 1
+SELECT id, owner, balance, currency, created_at
+FROM accounts
+WHERE id = $1
+LIMIT 1
 `
 
 func (q *Queries) GetAccount(ctx context.Context, id int64) (Accounts, error) {
@@ -57,9 +64,10 @@ func (q *Queries) GetAccount(ctx context.Context, id int64) (Accounts, error) {
 }
 
 const listAccounts = `-- name: ListAccounts :many
-SELECT id, owner, balance, currency, created_at FROM accounts
+SELECT id, owner, balance, currency, created_at
+FROM accounts
 ORDER BY id
-LIMIT $1
+LIMIT $1 
 OFFSET $2
 `
 
@@ -95,4 +103,29 @@ func (q *Queries) ListAccounts(ctx context.Context, arg ListAccountsParams) ([]A
 		return nil, err
 	}
 	return items, nil
+}
+
+const updateAccount = `-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING id, owner, balance, currency, created_at
+`
+
+type UpdateAccountParams struct {
+	ID      int64 `json:"id"`
+	Balance int64 `json:"balance"`
+}
+
+func (q *Queries) UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Accounts, error) {
+	row := q.db.QueryRowContext(ctx, updateAccount, arg.ID, arg.Balance)
+	var i Accounts
+	err := row.Scan(
+		&i.ID,
+		&i.Owner,
+		&i.Balance,
+		&i.Currency,
+		&i.CreatedAt,
+	)
+	return i, err
 }
