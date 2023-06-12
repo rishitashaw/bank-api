8d369332c3f394e2bd686ca5b539c12cd9c8d05f
Rishita Shaw <rishitashaw49@gmail.com>
2023-06-12 18:34:31 +0530

    list acc

--- api/account.go
+++ api/account.go
@@ -17,7 +17,7 @@ type createAccountRequest struct {
 func (server *Server) createAccount(ctx *gin.Context) {
 	var req createAccountRequest
 	if err:=ctx.ShouldBindJSON(&req); err!=nil{
-		ctx.JSON(http.StatusBadRequest, errorRequest(err))
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
@@ -30,7 +30,7 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, arg)
 
 	if err != nil{
-		ctx.JSON(http.StatusBadRequest, errorRequest(err))
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 	ctx.JSON(http.StatusOK,account)
@@ -44,17 +44,43 @@ func (server *Server) getAccount(ctx *gin.Context){
 	var req getAccountRequest
 
 	if err:=ctx.ShouldBindUri(&req); err!=nil{
-		ctx.JSON(http.StatusBadRequest, errorRequest(err))
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 	account, err:=server.store.GetAccount(ctx,req.ID)
 	if err != nil{
 		if err==sql.ErrNoRows{
-			ctx.JSON(http.StatusNotFound, errorRequest(err))
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
-		ctx.JSON(http.StatusInternalServerError, errorRequest(err))
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 	ctx.JSON(http.StatusOK,account)
+}
+
+type listAccountRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
 }
--- api/server.go
+++ api/server.go
@@ -18,6 +18,8 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
 
 	server.router=router
 	return server
@@ -27,6 +29,6 @@ func (server *Server) Start(address string) error {
 	return server.router.Run(address)
 }
 
-func errorRequest(err error) gin.H{
+func errorResponse(err error) gin.H{
 	return gin.H{"error": err.Error()}
 }
