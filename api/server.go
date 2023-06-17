93baa30edd158afeb4991b4234eb0fb2aee1f418
Rishita Shaw <rishitashaw49@gmail.com>
2023-06-17 19:30:21 +0530

    transfer api done

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/rishitashaw/bank-api/db/sqlc"
 )
 
@@ -17,6 +19,12 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfer", server.createTransfer)
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
 
 	server.router = router
 	return server
