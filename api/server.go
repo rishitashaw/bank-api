25d9d8a07ab518b6e4e805f5fec89b234868c7af
rishitashaw <rishitashaw49@gmail.com>
2023-06-22 22:05:09 +0530

    ok

--- api/server.go
+++ api/server.go
@@ -19,6 +19,8 @@ func NewServer(store db.Store) *Server {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
+	router.POST("/users", server.createUser)
+	router.GET("/users/:id", server.getUser)
 
 	router.POST("/transfer", server.createTransfer)
 
---
+++ api/user.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	db "github.com/rishitashaw/bank-api/db/sqlc"
+)
+
+type createUserRequest struct {
+	Username       string `json:"username" binding:"required"`
+	HashedPassword string `json:"hashed_password" binding:"required"`
+	FullName       string `json:"full_name" binding:"required"`
+	Email          string `json:"email" binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: req.HashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	}
+
+	account, err := server.store.CreateUser(ctx, arg)
+
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation", "foreign_key_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getUserRequest struct {
+	Username string `uri:"username" binding:"required"`
+}
+
+func (server *Server) getUser(ctx *gin.Context) {
+	var req getUserRequest
+
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
